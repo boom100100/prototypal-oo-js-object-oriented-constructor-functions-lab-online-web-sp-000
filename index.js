240fefe7393d093229f957b52b77114eb858d6f6
Bernadette Davis <bsd231@nyu.edu>
2020-02-04 14:12:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-let Scooter = function(){};
-let Driver = function(){};
+let Scooter = function(year, color, logo){
+  this.year = year;
+  this.color = color;
+  this.logo = logo;
+};
+let Driver = function(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+};
 let PickupLocation = function(){};
